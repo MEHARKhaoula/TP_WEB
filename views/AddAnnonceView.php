bade69cdc208015445697b384889420fb11f9c44
Khaoula Mehar <ik_mehar@esi.dz>
2021-12-24 18:03:01 +0100

    mcha

--- views/AddAnnonceView.php
+++ views/AddAnnonceView.php
@@ -1,12 +1,11 @@
 <?php
 
+require_once("../controller/AddAnnonceController.php") ;
 class AddAnnonceView
 {
     public function entetepage()
 {
-  
-    
-  echo'  <html>
+  echo'<html>
     <head>
       <link rel="stylesheet"  type="text/css" href="../menu.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -17,8 +16,8 @@ class AddAnnonceView
 
 public function afficher_form()
     {
-        
-     echo' <form action="controller/AddAnnonceController.php" method="Post" enctype="multipart/form-data">
+        ?>
+     <form action="" method="post" enctype="multipart/form-data">
       <label for="titre">Titre:</label><br>
         <input type="text" name="titre" placeholder="titre" /><br>
         <label for="description">Description:</label><br>
@@ -27,66 +26,100 @@ public function afficher_form()
         <input type="text" name="adressed"  /> <br>
         <label for="depart">Depart:</label><br>
         <SELECT name="depart" size="1"> 
-        <OPTION>Adrar
-        <OPTION>Chlef
-        <OPTION>Laghouat
-        <OPTION>Oum El Bouaghi
-        <OPTION>Batna
-        <OPTION>Bejaia
-        <OPTION>Biskra
-        <OPTION>Bechar
-        <OPTION>Blida
-        <OPTION>Bouira
+        <OPTION value="Adrar">Adrar
+        <OPTION value="Chlef">Chlef
+        <OPTION value="Laghouat">Laghouat
+        <OPTION value="Oum El Bouaghi">Oum El Bouaghi
+        <OPTION value="5">Batna
+        <OPTION value="6">Bejaia
+        <OPTION value="7">Biskra
+        <OPTION value="8">Bechar
+        <OPTION value="9">Blida
+        <OPTION value="10">Bouira
         </SELECT><br>
         <label for="adressea">Adresse arrivée:</label><br>
         <input type="text" name="adressea"  /> <br>
         <label for="arrive">Arrivee:</label><br>
         <SELECT name="arrive" size="1"> 
-        <OPTION>Adrar
-        <OPTION>Chlef
-        <OPTION>Laghouat
-        <OPTION>Oum El Bouaghi
-        <OPTION>Batna
-        <OPTION>Bejaia
-        <OPTION>Biskra
-        <OPTION>Bechar
-        <OPTION>Blida
-        <OPTION>Bouira
+        <OPTION value="1">Adrar
+        <OPTION value="2">Chlef
+        <OPTION value="3">Laghouat
+        <OPTION value="4">Oum El Bouaghi
+        <OPTION value="5">Batna
+        <OPTION value="6">Bejaia
+        <OPTION value="7">Biskra
+        <OPTION value="8">Bechar
+        <OPTION value="9">Blida
+        <OPTION value="10">Bouira
         </SELECT><br>
         <label for="type">Type transport:</label><br>
         <SELECT name="type" size="1">
-        <OPTION>colis
-        <OPTION>demenagement
-        <OPTION>lettre
+        <OPTION value="colis">colis
+        <OPTION value="demenagement">demenagement
+        <OPTION value="lettre">lettre
        
         </SELECT><br>
         <label for="moyen">Moyen transport:</label><br>
         <SELECT name="moyen" size="1">
-        <OPTION>voiture
-        <OPTION>camion
-        <OPTION>bus
+        <OPTION value="voiture">voiture
+        <OPTION value="camion">camion
+        <OPTION value="bus">bus
         </SELECT><br>
 
         <label for="poids">Poids:</label><br>
         <SELECT name="poids" size="1">
-        <OPTION> categorie1
-        <OPTION>categorie2
-        <OPTION>categorie3
+        <OPTION value="1"> categorie1
+        <OPTION value="2">categorie2
+        <OPTION value="3"> categorie3
         </SELECT><br>
 
         <label for="volume">Poids:</label><br>
         <SELECT name="volume" size="1">
-        <OPTION> categorie1
-        <OPTION>categorie2
-        <OPTION>categorie3
+        <OPTION value="1"> categorie1
+        <OPTION value="2">categorie2
+        <OPTION value="3">categorie3
         </SELECT><br>
 
         <input type="file" name="uploadfile" value=""/> <br>
 
-        <input type="submit" value="Publier" />
+        <input type="submit" value="Publier" name="submit"/>
 </form>
-</body>';
-    }
+</body>
+<?php
+if (isset($_POST['submit'])) { 
+
+  $titr=$_POST['titre'];
+  $desc=$_POST['description'];
+  $pointdepart =$_POST['adressed'];
+  $pointarrive=$_POST['adressea'];
+  $wilaya_depart=$_POST['depart'];
+  $wilaya_arrive=$_POST['arrive'];
+  $typetransport=$_POST['type'];
+  $poids_inf=40;
+  $poids_sup=60;
+  $moyen_transport=$_POST['moyen'];
+  $etat="valide" ;
+  $volume_inf=20;
+  $volume_sup=60;
+  $filename = $_FILES["uploadfile"]["name"];
+  $tempname = $_FILES["uploadfile"]["tmp_name"];
+  $folder = "../images/".$filename;	
+  
+  
+  
+  move_uploaded_file($tempname, $folder);
+  $c = new Annonce() ;
+  $c->add($titr,$desc,$pointdepart,$pointarrive,$wilaya_depart,$wilaya_arrive,$typetransport,$poids_inf,$poids_sup,$moyen_transport,$etat,$volume_inf,$volume_sup,$filename);
+  
+}
+  } 
+    public function envoyer(){
+        $c = new AddAnnonceController();
+        $c->envoyer_form() ;
+    } 
+    
 }
 
-    ?>
+    ?> 
+
+   
